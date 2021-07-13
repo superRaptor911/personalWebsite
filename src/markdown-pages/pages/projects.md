02506509750e8ff930b6af2e4a6f057f58932443
superRaptor911 <raptor.inc2018@gmail.com>
2021-07-13 22:50:37 +0530

    Added Markdown support

--- src/markdown-pages/pages/projects.md
+++ src/markdown-pages/pages/projects.md
@@ -5,6 +5,14 @@ title: "projects"
 
 # My Projects
 
+<br/>
 
+## [Splitit](https://github.com/superRaptor911/SplitIt) 
+
+![split it](https://user-images.githubusercontent.com/58220198/123923328-83904200-d9a6-11eb-9a17-233990ccee9d.png)
+
+A mobile App to split videos into 30 second cuts which can be easily shared saved or posted as WhatsApp status.
+
+[<img src="https://play.google.com/intl/en_us/badges/images/generic/en-play-badge.png" alt="Get it on Google Play" height="80">](https://play.google.com/store/apps/details?id=com.splitit)
 
 
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,16 +1,37 @@
+import { graphql } from 'gatsby';
 import React from 'react';
 import Header from '../components/Header';
 import '../styles/Global.css';
 
-const projects = () => {
+const projects = ({ data }) => {
+  const markdownRemark = data.allFile.nodes[0].childrenMarkdownRemark[0];
+  const { html } = markdownRemark;
   return (
     <div>
       <Header curPage="Projects"/>
       <div className="content">
-        Projects
+        <div
+          className="blog-post-content"
+          dangerouslySetInnerHTML={{ __html: html }}
+        />
       </div>
     </div>
   );
 };
+
+// eslint-disable-next-line no-unused-vars
+export const query = graphql`
+  {
+    allFile(
+      filter: {sourceInstanceName: {eq: "pages"}, childrenMarkdownRemark: {elemMatch: {frontmatter: {slug: {eq: "projects"}}}}}
+    ) {
+      nodes {
+        childrenMarkdownRemark {
+          html
+        }
+      }
+    }
+  }
+`;
 
 export default projects;
